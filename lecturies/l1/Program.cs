502acccb56a65155ce653c2e7b6e8e8e8c909698
Владислав Сердюк <fan.of.Dnipro@gmail.com>
2017-03-25 21:21:58 +0200

    First

---
+++ lecturies/l1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList list = new MyList();
+            Console.WriteLine(list[3]);
+        }
+        class MyClass
+        {
+            private string firstName;
+            private string lastName;
+            public string Name
+            {
+                get
+                {
+                    return firstName + " " + lastName;
+                }
+            }
+        }
+        class MyList
+        {
+            public int this [int index]
+            {
+                get
+                {
+                    return 0;
+                }
+                set
+                {
+
+                }
+            }
+            Node root;
+            public class Node
+            {
+                public int Data { get; set; }
+                public Node Next { get; set; }
+            }
+        }
+    }
+}
---
+++ lecturies/l2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace l2
+{
+    [XmlRoot(ElementName ="catalog")]
+    public class Catalog 
+    {
+        [XmlElement(ElementName ="items")]
+        public CatalogItemsCollection Items { get; set; }
+    }
+    public class CatalogItemsCollection
+    {
+        [XmlElement(ElementName ="item")]
+        public List<CatalogItem> Items { get; set; }
+    }
+    public class CatalogItem
+    {
+        [XmlElement(ElementName ="MagazineID")]
+        public string MagazineID
+        {
+            get;set;
+        }
+        [XmlElement(ElementName = "PublishDate")]
+        public string PublishDate { get; set; }
+        [XmlElement(ElementName = "ImageSmall")]
+        public string ImageSmall { get; set; }
+        [XmlElement(ElementName = "ImageLarge")]
+        public string ImageLarge { get; set; }
+        [XmlElement(ElementName = "ImageSmall2x")]
+        public string ImageSmall2x { get; set; }
+        [XmlElement(ElementName = "ImageLarge2x")]
+        public string ImageLarge2x { get; set; }
+    }
+}
